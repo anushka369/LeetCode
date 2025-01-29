8aa8dc24414ae9ff55bc145399f1d3809d686550
anushka <81908360+anushka369@users.noreply.github.com>
2025-01-29 18:21:43 +0530

    Create 6. Zigzag Conversion.py

---
+++ 6. Zigzag Conversion.py
@@ -0,0 +1,14 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        g = [[] for _ in range(numRows)]
+        i, k = 0, -1
+        for c in s:
+            g[i].append(c)
+            if i == 0 or i == numRows - 1:
+                k = -k
+            i += k
+        return ''.join(chain(*g))
+
+# Link to the problem: https://leetcode.com/problems/zigzag-conversion/
