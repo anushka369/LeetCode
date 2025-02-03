6174d9f1242bb3aa521de4be7667a1144c88d11f
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-03 15:14:48 +0530

    Create 0077. Combinations.py

---
+++ 0001-0199/0077. Combinations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def dfs(i: int):
+            if len(t) == k:
+                ans.append(t[:])
+                return
+              
+            if i > n:
+                return
+              
+            t.append(i)
+            dfs(i + 1)
+            t.pop()
+            dfs(i + 1)
+
+        ans = []
+        t = []
+        dfs(1)
+        return ans
+
+# Link to the problem: https://leetcode.com/problems/combinations/
