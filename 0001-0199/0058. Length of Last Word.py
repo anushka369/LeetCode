9ffed13bcb14af6bcf032bc79b2ce798260c6164
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-03 14:55:10 +0530

    Create 0058. Length of Last Word.py

---
+++ 0001-0199/0058. Length of Last Word.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i = len(s) - 1
+      
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+        j = i
+      
+        while j >= 0 and s[j] != ' ':
+            j -= 1
+        return i - j
+
+# Link to the problem: https://leetcode.com/problems/length-of-last-word/
