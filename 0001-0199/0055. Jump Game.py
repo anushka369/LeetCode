1d34cf147af86e5f08e3a5267141d04bfae94f27
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-03 14:49:54 +0530

    Create 0055. Jump Game.py

---
+++ 0001-0199/0055. Jump Game.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        mx = 0
+        for i, x in enumerate(nums):
+            if mx < i:
+                return False
+            mx = max(mx, i + x)
+        return True
+
+# Link to the problem: https://leetcode.com/problems/jump-game/
