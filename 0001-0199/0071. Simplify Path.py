df676786f74a7de2d9a91e6c86b7620552b7b7ff
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-03 15:08:40 +0530

    Create 0071. Simplify Path.py

---
+++ 0001-0199/0071. Simplify Path.py
@@ -0,0 +1,16 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stk = []
+      
+        for s in path.split('/'):
+            if not s or s == '.':
+                continue
+            if s == '..':
+                if stk:
+                    stk.pop()
+            else:
+                stk.append(s)
+              
+        return '/' + '/'.join(stk)
+
+# Link to the problem: https://leetcode.com/problems/simplify-path/
