ed1a3be69eb13ed99e2b81b302a3e07afa169833
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-05 14:09:32 +0530

    Create 0118. Pascal's Triangle.py

---
+++ 0100-0199/0118. Pascal's Triangle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        f = [[1]]
+      
+        for i in range(numRows - 1):
+            g = [1] + [a + b for a, b in pairwise(f[-1])] + [1]
+            f.append(g)
+        return f
+
+# Link to the problem: https://leetcode.com/problems/pascals-triangle/
