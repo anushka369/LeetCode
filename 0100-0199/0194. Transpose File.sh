693052cfde8f7dc3639ac4c003e71095d8487a29
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-08 00:59:13 +0530

    Create 0194. Transpose File.sh

---
+++ 0100-0199/0194. Transpose File.sh
@@ -0,0 +1,28 @@
+# Read from the file file.txt and print its transposed content to stdout.
+
+awk '
+{
+    for (i = 1; i <= NF; i++) 
+    {
+        if (NR == 1) 
+        {
+            res[i] = re$i
+        } 
+        
+        else 
+        {
+            res[i] = res[i]" "$i
+        }
+    }
+}
+
+END 
+{
+    for (i = 1; i <= NF; i++) 
+    {
+        print res[i]
+    }
+}
+' file.txt
+
+# Link to the problem: https://leetcode.com/problems/transpose-file/
