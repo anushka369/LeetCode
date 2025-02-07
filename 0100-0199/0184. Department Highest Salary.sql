d0b1ef2cb3e6012b497fc0e4768e8f1a747edbd4
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-08 00:50:06 +0530

    Create 0184. Department Highest Salary.sql

---
+++ 0100-0199/0184. Department Highest Salary.sql
@@ -0,0 +1,15 @@
+SELECT d.name AS department, 
+       e.name AS employee, 
+       salary
+  
+FROM Employee AS e
+JOIN Department AS d 
+ON e.departmentId = d.id
+  
+WHERE (d.id, salary) IN 
+(
+    SELECT departmentId, MAX(salary)
+    FROM Employee GROUP BY 1
+);
+
+-- Link to the problem: https://leetcode.com/problems/department-highest-salary/
