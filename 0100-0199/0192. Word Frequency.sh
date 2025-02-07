9602eb8565cd3701770ed8a2b2c3ed43253d7ad7
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-08 00:57:10 +0530

    Update 0192. Word Frequency.sh

--- 0100-0199/0192. Word Frequency.sh
+++ 0100-0199/0192. Word Frequency.sh
@@ -1,3 +1,5 @@
 # Read from the file words.txt and output the word frequency list to stdout.
 
 cat words.txt | tr -s ' ' '\n' | sort | uniq -c | sort -nr | awk '{print $2, $1}'
+
+# Link to the problem: https://leetcode.com/problems/word-frequency/
