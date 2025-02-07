f065c0db7ff5fac523b0f9524829c722f940a0f7
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-07 07:52:43 +0530

    Create 0152. Maximum Product Subarray.py

---
+++ 0100-0199/0152. Maximum Product Subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        ans = f = g = nums[0]
+      
+        for x in nums[1:]:
+            ff, gg = f, g
+            f = max(x, ff * x, gg * x)
+            g = min(x, ff * x, gg * x)
+            ans = max(ans, f)
+        return ans
+
+# Link to the problem: https://leetcode.com/problems/maximum-product-subarray/
