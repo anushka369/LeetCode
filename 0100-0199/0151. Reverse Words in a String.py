deb756b962195fee713d18b7428149a3c0a3130b
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-07 07:46:39 +0530

    Create 0151. Reverse Words in a String.py

---
+++ 0100-0199/0151. Reverse Words in a String.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = []
+        i, n = 0, len(s)
+      
+        while i < n:
+            while i < n and s[i] == " ":
+                i += 1
+              
+            if i < n:
+                j = i
+              
+                while j < n and s[j] != " ":
+                    j += 1
+                  
+                words.append(s[i:j])
+                i = j
+              
+        return " ".join(words[::-1])
+
+# Link to the problem: https://leetcode.com/problems/reverse-words-in-a-string/
