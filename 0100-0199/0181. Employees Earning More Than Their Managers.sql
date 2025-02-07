ffc7bbd798c9f96a9070d89aa6b6f50a81606af7
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-08 00:45:36 +0530

    Create 0181. Employees Earning More Than Their Managers.sql

---
+++ 0100-0199/0181. Employees Earning More Than Their Managers.sql
@@ -0,0 +1,5 @@
+SELECT e1.name Employee FROM Employee e1
+JOIN Employee e2 ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary;
+
+-- Link to the problem: https://leetcode.com/problems/employees-earning-more-than-their-managers/
