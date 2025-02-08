58130f100f440823437edf51e009deb6b5e8e2c6
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-09 04:10:30 +0530

    Create 0209. Minimum Size Subarray Sum.py

---
+++ 0200-0299/0209. Minimum Size Subarray Sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        n = len(nums)
+        s = list(accumulate(nums, initial=0))
+        ans = n + 1
+      
+        for i, x in enumerate(s):
+            j = bisect_left(s, x + target)
+            if j <= n:
+                ans = min(ans, j - i)
+        return ans if ans <= n else 0
+
+# Link to the problem: https://leetcode.com/problems/minimum-size-subarray-sum/
