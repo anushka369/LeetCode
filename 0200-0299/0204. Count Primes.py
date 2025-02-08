83c447efb3e8a8f6e3de9712e34fa4672904dc43
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-09 04:03:03 +0530

    Create 0204. Count Primes.py

---
+++ 0200-0299/0204. Count Primes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        primes = [True] * n
+        ans = 0
+      
+        for i in range(2, n):
+            if primes[i]:
+                ans += 1
+                for j in range(i + i, n, i):
+                    primes[j] = False
+        return ans
+
+# Link to the problem: https://leetcode.com/problems/count-primes/
