786c03505dd8cdc41ad2447c7dbef282a4bbf9f1
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-09 04:01:29 +0530

    Create 0202. Happy Number.py

---
+++ 0200-0299/0202. Happy Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        vis = set()
+      
+        while n != 1 and n not in vis:
+            vis.add(n)
+            x = 0
+          
+            while n:
+                n, v = divmod(n, 10)
+                x += v * v
+            n = x
+        return n == 1
+
+# Link to the problem: https://leetcode.com/problems/happy-number/
