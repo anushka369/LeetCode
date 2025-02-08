86fc607eca2c2e9fa3511e09dbcc119a039be7a8
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-09 04:52:20 +0530

    Create 0227. Basic Calculator II.py

---
+++ 0200-0299/0227. Basic Calculator II.py
@@ -0,0 +1,29 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        v, n = 0, len(s)
+        sign = '+'
+        stk = []
+      
+        for i, c in enumerate(s):
+            if c.isdigit():
+                v = v * 10 + int(c)
+              
+            if i == n - 1 or c in '+-*/':
+                match sign:
+                    case '+':
+                        stk.append(v)
+                      
+                    case '-':
+                        stk.append(-v)
+                      
+                    case '*':
+                        stk.append(stk.pop() * v)
+                      
+                    case '/':
+                        stk.append(int(stk.pop() / v))
+                      
+                sign = c
+                v = 0
+        return sum(stk)
+
+# Link to the problem: https://leetcode.com/problems/basic-calculator-ii/
