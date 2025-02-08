5833b1003bf5eedbdddd127d0f342f053471fa36
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-09 05:00:25 +0530

    Create 0233. Number of Digit One.py

---
+++ 0200-0299/0233. Number of Digit One.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countDigitOne(self, n: int) -> int:
+        @cache
+        def dfs(i: int, cnt: int, limit: bool) -> int:
+            if i >= len(s):
+                return cnt
+              
+            up = int(s[i]) if limit else 9
+            ans = 0
+          
+            for j in range(up + 1):
+                ans += dfs(i + 1, cnt + (j == 1), limit and j == up)
+            return ans
+
+        s = str(n)
+        return dfs(0, 0, True)
+
+# Link to the problem: https://leetcode.com/problems/number-of-digit-one/
