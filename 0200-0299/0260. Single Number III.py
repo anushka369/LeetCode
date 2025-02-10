bea3ecd1cd8f5cc3d0ce9b5b42f32968d4036b3a
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-10 23:53:41 +0530

    Create 0260. Single Number III.py

---
+++ 0200-0299/0260. Single Number III.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xs = reduce(xor, nums)
+        a = 0
+        lb = xs & -xs
+      
+        for x in nums:
+            if x & lb:
+                a ^= x
+              
+        b = xs ^ a
+        return [a, b]
+
+# Link to the problem: https://leetcode.com/problems/single-number-iii/
