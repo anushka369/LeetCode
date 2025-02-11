5ada195e6533c9ac5367ca2fa06e5df8b70a65ee
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-11 14:01:31 +0530

    Create 0275. H-Index II.py

---
+++ 0200-0299/0275. H-Index II.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        left, right = 0, n
+      
+        while left < right:
+            mid = (left + right + 1) >> 1
+          
+            if citations[n - mid] >= mid:
+                left = mid
+              
+            else:              
+                right = mid - 1
+        return left
+
+# Link to the problem: https://leetcode.com/problems/h-index-ii/
