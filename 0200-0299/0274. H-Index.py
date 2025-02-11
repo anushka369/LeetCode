7b8c51af461a950c5a5ea97380d5763010154fb2
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-11 14:00:22 +0530

    Create 0274. H-Index.py

---
+++ 0200-0299/0274. H-Index.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+      
+        for h in range(len(citations), 0, -1):
+            if citations[h - 1] >= h:
+                return h
+        return 0
+
+# Link to the problem: https://leetcode.com/problems/h-index/
