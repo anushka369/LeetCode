251089dd64cffedff6e606960ce0de9186a0b864
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-11 14:05:42 +0530

    Create 0283. Move Zeroes.py

---
+++ 0200-0299/0283. Move Zeroes.py
@@ -0,0 +1,10 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        k = 0
+      
+        for i, x in enumerate(nums):
+            if x:
+                nums[k], nums[i] = nums[i], nums[k]
+                k += 1
+
+# Link to the problem: https://leetcode.com/problems/move-zeroes/
