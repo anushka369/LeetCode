aed28773d8932bccda3166698209685c55881c7e
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-11 14:28:23 +0530

    Create 0299. Bulls and Cows.py

---
+++ 0200-0299/0299. Bulls and Cows.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        cnt1, cnt2 = Counter(), Counter()
+        x = 0
+      
+        for a, b in zip(secret, guess):
+            if a == b:
+                x += 1
+              
+            else:
+                cnt1[a] += 1
+                cnt2[b] += 1
+              
+        y = sum(min(cnt1[c], cnt2[c]) for c in cnt1)
+        return f"{x}A{y}B"
+
+# Link to the problem: https://leetcode.com/problems/bulls-and-cows/
