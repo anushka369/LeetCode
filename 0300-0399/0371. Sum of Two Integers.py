7a6b3fbddd629db3ca2be252902c5ceb4e1e2435
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-13 08:58:18 +0530

    Create 0371. Sum of Two Integers.py

---
+++ 0300-0399/0371. Sum of Two Integers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        a, b = a & 0xFFFFFFFF, b & 0xFFFFFFFF
+      
+        while b:
+            carry = ((a & b) << 1) & 0xFFFFFFFF
+            a, b = a ^ b, carry
+        return a if a < 0x80000000 else ~(a ^ 0xFFFFFFFF)
+
+# Link to the problem: https://leetcode.com/problems/sum-of-two-integers/
