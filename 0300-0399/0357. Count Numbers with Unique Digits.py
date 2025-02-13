9b2b0abaf242b759563047f7b16799403f9729e2
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-13 08:49:46 +0530

    Create 0357. Count Numbers with Unique Digits.py

---
+++ 0300-0399/0357. Count Numbers with Unique Digits.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countNumbersWithUniqueDigits(self, n: int) -> int:
+        @cache
+        def dfs(i: int, mask: int, lead: bool) -> int:
+            if i < 0:
+                return 1
+            ans = 0
+          
+            for j in range(10):
+                if mask >> j & 1:
+                    continue
+                  
+                if lead and j == 0:
+                    ans += dfs(i - 1, mask, True)
+                  
+                else:
+                    ans += dfs(i - 1, mask | 1 << j, False)
+            return ans
+        return dfs(n - 1, 0, True)
+
+# Link to the problem: https://leetcode.com/problems/count-numbers-with-unique-digits/
