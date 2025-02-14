90a2d7309c43fc794263b404f80d8776ea4851d4
anushka <81908360+anushka369@users.noreply.github.com>
2025-02-14 14:21:52 +0530

    Create 0383. Ransom Note.py

---
+++ 0300-0399/0383. Ransom Note.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        cnt = Counter(magazine)
+      
+        for c in ransomNote:
+            cnt[c] -= 1
+          
+            if cnt[c] < 0:
+                return False
+        return True
+
+# Link to the problem: https://leetcode.com/problems/ransom-note/
